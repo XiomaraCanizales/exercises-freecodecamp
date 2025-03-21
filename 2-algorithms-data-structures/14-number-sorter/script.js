00ffb666963ded1b957c03e8242cd7570dbbbac3
Xiomara Canizales <53951477+XiomaraCanizales@users.noreply.github.com>
2025-03-20 23:14:23 -0600

    adding 14th course

---
+++ 2-algorithms-data-structures/14-number-sorter/script.js
@@ -0,0 +1,59 @@
+const sortButton = document.getElementById("sort")
+const sortInputArray = (event) => {
+    event.preventDefault()
+  
+    const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => dropdown.value)
+    
+    //const sortedValues = bubbleSort(inputValues)
+    const sortedValues = selectionSort(inputValues)
+    
+   //updateUI(inputValues)
+    updateUI(sortedValues)
+}
+
+const updateUI = (array = []) => {
+    array.forEach((num, i) => {
+        const outputValueNode = document.getElementById(`output-value-${i}`)
+        outputValueNode.innerText = num
+    })
+}
+
+// BUBBLE SORT
+const bubbleSort = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array.length - 1; j++) {
+        console.log(array, array[j], array[j + 1]);
+  
+        if (array[j] > array[j + 1]) {
+          const temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+        }
+      }
+    }
+  
+    return array;
+}
+
+// SELECTION SORT
+const selectionSort = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      let minIndex = i;
+  
+      for (let j = i + 1; j < array.length; j++) {
+        console.log(array, array[j], array[minIndex]);
+        if (array[j] < array[minIndex]) {
+          minIndex = j;
+        }
+      }
+  
+      const temp = array[i];
+      array[i] = array[minIndex];
+      array[minIndex] = temp;
+    }
+  
+  return array
+  
+}
+
+sortButton.addEventListener("click", sortInputArray)
